04469b4128578d4a0bddb1e3bbebbc2aa856a92a
Jacobus Geluk <jacobus.geluk@agnos.ai>
2021-01-16 17:57:37 +0000

    kg- now everywhere ekg- and some other renames, glossaries work now

--- latex-lib/build.sh
+++ latex-lib/build.sh
@@ -444,8 +444,8 @@ function copyToGoogleDriveLocal() {
         ekg-method)
           targetDirectory+="/service-group/ekg-method"
           ;;
-        ekg-discover)
-          targetDirectory+="/service-group/ekg-discover"
+        ekg-usecases)
+          targetDirectory+="/service-group/ekg-usecases"
           ;;
         *)
           targetDirectory+="/service-group/generated-docs"
