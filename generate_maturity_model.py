c573391998a0e0c7ea73dcd1f788d28e3da7b810
Jacobus Geluk <jacobus.geluk@agnos.ai>
2022-09-14 13:17:00 +0100

    Deployed 8e962fe with MkDocs version: 1.3.1

--- generate_maturity_model.py
+++ generate_maturity_model.py
@@ -5,8 +5,8 @@ from pathlib import Path
 config = ekglib.maturity_model_parser.Config(
     verbose=False,
     mkdocs=False,
-    model_name="EKG/MM",
-    model_root=Path('../ekg-mm').resolve(),
+    model_name="EKG/Maturity",
+    model_root=Path('../ekg-maturity').resolve(),
     output_root=Path("./docs").resolve(),
     docs_root=Path(os.getcwd()) / "docs",
     fragments_root=Path(os.getcwd()) / "docs-fragments"    
